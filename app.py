d2427a3d971396a7d8df0c970f176936c1d6fe55
darthbeep <darthbeep@gmail.com>
2017-10-27 00:53:08 -0400

    added newstory.html and update.html

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, session, url_for, redirect, render_template, request
-
+from util.table_helper import *
+import sqlite3
 import os
 
 #App instantiation
@@ -22,24 +23,24 @@ def logged(user = ""):
 		username = request.args["user"].lower()
 		password = request.args["password"]
 		print "BOUTTA CHECK THAT USERNAME \n\n"
-		if username == "test" #in the database: 
+		if username == "test": #in the database:
 			print "THE USERNAME HAS BEEN VALIDATED \n\n"
-			if password == "abc123":  #in the database: 
+			if password == "abc123":  #in the database:
 			        print "THE PASSWORD HAS BEEN VALIDATED \n\n"
-				session["username"] = #user_username
+				session["username"] = user_username
 				print "Does we makes it?\n\n"
 				return redirect("/homepage")
 			else:
-			        flash("Incorrect password, please try again.") 
+			        flash("Incorrect password, please try again.")
 		else:
-			flash("Incorrect username, please try again.") 
+			flash("Incorrect username, please try again.")
 
 	#If you already have a username, it brings you here
 	#WHAT DOES THIS DO
         else:
-		return redirect("/listofstories") 
+		return redirect("/listofstories")
 
-@app.route('/shainatesting')
+@app.route('/shainatesting') #Phasing this out for actual routes
 def testing():
 	testmode = "list"
 	if testmode == "read":
@@ -50,8 +51,31 @@ def testing():
 		return render_template('listofstories.html', stories=[1,2,3,4,5,6,7,8,9])
 	return "nope"
 
-@app.route("/register")
+@app.route('/readstory')
+def readstory(updates=None):
+    if updates == None:
+		updates = [{'user':"Caligula", 'text': 'idk roman emperors are cool'}, {'user':'alexander', 'text':'he was kind of great'}]
+    return render_template('read.html',title="this is a title", updates=updates)
+
+#This is the function for validating the new user
+@app.route('/register_redirect', methods=["GET", "POST"])
+def register_redirect():
+    user = request.form["user"]
+    email = request.form["email"]
+    passo = request.form["passo"]
+    rpasso = request.form["repeatpasso"]
+	#Still needs work
+    if add_user(user, passo, 'first_name', 'last_name', email):
+		return redirect("/register")
+    else:
+		return 'something went wrong'
+    return user + ", " + email + ", " + passo + ", " + rpasso
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
+	if request.method=="POST":
+		return request.form['user']
+	return render_template('register.html')
         #If form has been submited:
         #blah
 
@@ -61,20 +85,36 @@ def register():
         #Else:
         #return render_template("register.html")
 
-'''@app.route("/wrong")
-def u_messed_up():
-    return render_template("errorpage.html", bad = request.args.get("err"))'''
+#'''@app.route("/wrong")
+#def u_messed_up():
+#    return render_template("errorpage.html", bad = request.args.get("err"))'''
 
-app.route("/homepage")
+@app.route("/homepage")
 def homepage():
+	pass
         #return render_template('homepage.html')
 
 
-#IF user chooses to read stories:         
-app.route("/listofstories") 
+#IF user chooses to read stories:
+@app.route("/listofstories")
 def read():
-        return render_template('listofstoriess.html', stories=[1,2,3,4,5,6,7,8,9])
+        return render_template('listofstories.html', stories=[1,2,3,4,5,6,7,8,9])
 
+@app.route('/createstory',methods=["GET", "POST"])
+def createstory():
+	if request.method == "POST":
+		return render_template('success.html',whathappened="created")
+	return render_template("newstory.html")
+
+@app.route('/updatestory', methods=["GET", "POST"]) #I didn't copy create, I just rewrote it
+def updatestory():
+	samplestory = {
+		'title':'once upon a time',
+		'text':'Ipsum tempor elit culpa cupidatat quis et laborum tempor nostrud voluptate nisi cupidatat. Ad fugiat sit laborum in consectetur ullamco ut esse. Ut eiusmod aliquip minim commodo id deserunt officia magna anim ut veniam ipsum laborum. Lorem duis ea elit ullamco sint est laborum enim sint.'
+	}
+	if request.method == "POST":
+		return render_template('success.html', whathappened="updated")
+	return render_template("update.html",story=samplestory)
 
 if __name__ == "__main__":
     app.debug = True
--- util/table_helper.py
+++ util/table_helper.py
@@ -1,44 +1,56 @@
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
-from hashlib import shai1 #hashing passwords
-f="/data/bestsite.db"
+from hashlib import *
+#from hashlib import shai1 #hashing passwords
+#f="/data/bestsite.db"
 
-db = sqlite3.connect(f) #open if f exists, otherwise create
-c = db.cursor()    #facilitate db ops
+def setup():
+    f="/data/bestsite.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
+
+#db = sqlite3.connect(f) #open if f exists, otherwise create
+#c = db.cursor()    #facilitate db ops
 ######################
 #dict builder
 def dict_user_pass():
+    f="/data/bestsite.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
     q = "SELECT * FROM users"
     foo = c.execute(q)
     users = {}
     for bar in foo:
         user = bar[0]
         password = bar[1]
-        users[user] = password;       
-return users
+        users[user] = password;
+    db.commit()
+    db.close()
+    return users
 ##################
 def add_user(username, password, first_name, last_name, email):
+    f="bestsite.db"
+    db = sqlite3.connect(f)
+    c = db.cursor()
     userpass = dict_user_pass()
     passw = sha1(password).hexdigest()
     if (username in userpass):
         return False
     command = "INSERT INTO users VALUES('"+ username + "','" + passw + "','" + first_name + "', '" + last_name + "','" + email + "', none)"
     c.execute(command)
+    db.commit()
+    db.close()
     return True
 ####################
 def verify_user(username, password):
     userpass = dict_user_pass()
-    if !(username in userpass):
+    if not(username in userpass):
         return False
     if(userpass[username] == password):
         return True
-    return False 
+    return False
 
 
 #==========================================================
-db.commit() #save changes
-db.close()  #close database'''
-
-    
-    
-    
+#db.commit() #save changes
+#db.close()  #close database'''
